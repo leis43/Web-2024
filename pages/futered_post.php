e1554465bff830a2e41c90ecd739b7ddce274ef2
leis43 <159579625+leis43@users.noreply.github.com>
2024-03-22 10:52:22 +0300

    end.

---
+++ pages/futered_post.php
@@ -0,0 +1,28 @@
+<a class="news__post" href='/post?id=<?= $post['id'] ?>' style="background-image: url('<?= $post["img_modifier"] ?>');
+        background-size: cover; ">
+  <?php if ($post['id'] != 1): ?>
+    <span class="news-post__button">
+      <?= $post['button'] ?>
+    </span>
+  <?php endif; ?>
+
+  <div class="news-post__description">
+    <h3 class="news-post__title">
+      <?= $post['title'] ?>
+    </h3>
+    <span class="news-post__subtitle">
+      <?= $post['subtitle'] ?>
+    </span>
+    <div class="news-post__info">
+      <div class="news-post-info__autor">
+        <img class="news-post-info-autor__avatar" src="<?= $post['img_autor'] ?>" alt="img">
+        <span class="news-post-info-autor__name">
+          <?= $post['author'] ?>
+        </span>
+      </div>
+      <span class="news-post-info__release-date">
+        <?= $post['posting_data'] ?>
+      </span>
+    </div>
+  </div>
+</a>
--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,92 @@
+
+<?php
+$most_posts = [
+	[
+		'id' => 3,
+		'title' => 'Still Standing Tall',
+		'subtitle' => 'Life begins at the end of your comfort zone.',
+		'img_modifier' => './static/images/images__home/SST.jpg',
+		'author' => 'William Wong',
+		'img_autor' => './static/images/images__home/ManWW.png',
+		'posting_data' => '9/25/2015',
+
+	],
+	[
+		'id' => 4,
+		'title' => 'Sunny Side Up',
+		'subtitle' => 'No place is ever as bad as they tell you it’s going to be.',
+		'img_modifier' => './static/images/images__home/SSU.png',
+		'author' => 'Mat Vogels',
+		'img_autor' => './static/images/images__home/ManMV.png',
+		'posting_data' => '9/25/2015',
+	],
+	[
+		'id' => 5,
+		'title' => 'Water Falls',
+		'subtitle' => 'We travel not to escape life, but for life not to escape us',
+		'img_modifier' => './static/images/images__home/WF.png',
+		'author' => 'Mat Vogels',
+		'img_autor' => './static/images/images__home/ManMV.png',
+		'posting_data' => '9/25/2015',
+	],
+	[
+		'id' => 6,
+		'title' => 'Through the Mist',
+		'subtitle' => 'Travel makes you see what a tiny place you occupy in the world.',
+		'img_modifier' => './static/images/images__home/TTM.png',
+		'author' => 'Mat Vogels',
+		'img_autor' => './static/images/images__home/ManWW.png',
+		'posting_data' => '9/25/2015',
+	],
+	[
+		'id' => 7,
+		'title' => 'Awaken Early',
+		'subtitle' => 'Not all those who wander are lost.',
+		'img_modifier' => './static/images/images__home/AE.png',
+		'author' => 'Mat Vogels',
+		'img_autor' => './static/images/images__home/ManMV.png',
+		'posting_data' => '9/25/2015',
+	],
+	[
+		'id' => 8,
+		'title' => 'Try it Always',
+		'subtitle' => 'The world is a book, and those who do not travel read only one page.',
+		'img_modifier' => './static/images/images__home/TIA.png',
+		'author' => 'Mat Vogels',
+		'img_autor' => './static/images/images__home/ManMV.png',
+		'posting_data' => '9/25/2015',
+	],
+];
+
+$futered_posts = [
+	[
+		'id' => 1,
+		'title' => 'The Road Ahead',
+		'subtitle' => 'The road ahead might be paved - it might not be.',
+		'img_modifier' => './static/images/images__home/TRA.png',
+		'author' => 'Mat Vogels',
+		'img_autor' => './static/images/images__home/ManMV.png',
+		'posting_data' => 'September 25, 2015',
+	],
+
+	[
+		'id' => 2,
+		'button' => 'ADVENTURE',
+		'title' => 'From Top Down',
+		'subtitle' => 'Once a year, go someplace you’ve never been before.',
+		'img_modifier' => './static/images/images__home/FTD.png',
+		'author' => 'William Wong',
+		'img_autor' => './static/images/images__home/ManWW.png',
+		'posting_data' => 'September 25, 2015',
+	],
+];
+?>
 <!doctype html>
 <html lang="en">
 
 <head>
 	<title>Let\'s do it together.</title>
-	<link type="text/css" rel="stylesheet" href="./static/styles/style2.css">
-	<link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="./static/styles/style2-1.css">
 	<link
 		href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Oxygen:wght@300;400;700&display=swap"
 		rel="stylesheet">
@@ -14,7 +96,9 @@
 <body>
 	<header class="header">
 		<div class="navigation">
-			<a class="navigation__logo" href="#">Escape.</a>
+			<a class="navigation__logo" href="#">
+				<img src="./static/images/images__home/Escape1.svg">
+			</a>
 			<ul class="navigation__list">
 				<li>
 					<a class="navigation__link" href="#">HOME</a>
@@ -36,152 +120,56 @@
 			<a href="#" class="header__button">View Latest Posts </a>
 		</div>
 	</header>
-	<div class="menu">
-		<a href="#" class="menu__link">Nature</a>
-		<a href="#" class="menu__link">Photography</a>
-		<a href="#" class="menu__link">Relaxation</a>
-		<a href="#" class="menu__link">Vacation</a>
-		<a href="#" class="menu__link">Travel</a>
-		<a href="#" class="menu__link">Adventure</a>
-	</div>
+	<ul class="menu">
+		<li>
+			<a href="#" class="menu__link">Nature</a>
+		</li>
+		<li>
+			<a href="#" class="menu__link">Photography</a>
+		</li>
+		<li>
+			<a href="#" class="menu__link">Relaxation</a>
+		</li>
+		<li>
+			<a href="#" class="menu__link">Vacation</a>
+		</li>
+		<li>
+			<a href="#" class="menu__link">Travel</a>
+		</li>
+		<li>
+			<a href="#" class="menu__link">Adventure</a>
+		</li>
+	</ul>
 	<main class="content">
 		<div class="section-headings">
 			<span class="section-headings__title">Featured Posts</span>
 		</div>
 		<div class="news">
-			<div class="news__post">
-				<img class="news-post__background" src="./static/images/images__home/TRA.png" alt="img">
-				<div class="news-post__description">
-					<a href="#" class="news-post__title">The Road Ahead</a>
-					<span class="news-post__subtitle">The road ahead might be paved - it might not be.</span>
-					<div class="news-post__info">
-						<div class="news-post-info__autor">
-							<img class="news-post-info-autor__avatar" src="./static/images/images__home/ManMV.png" alt="img">
-							<span class="news-post-info-autor__name">Mat Vogels</span>
-						</div>
-						<span class="news-post-info__release-date">September 25, 2015</span>
-					</div>
-				</div>
-			</div>
-			<div class="news__post-with-categories">
-				<img class="news-post-with-categories__background" src="./static/images/images__home/FTD.png" alt="img">
-				<div class="button">
-					<a href="#" class="button__categories-of-post">ADVENTURE</a>
-				</div>
-				<div class="news-post-with-categories__description">
-					<a href="#" class="news-post__title">From Top Down</a>
-					<span class="news-post__subtitle">Once a year, go someplace you’ve never been before.</span>
-					<div class="news-post__info">
-						<div class="news-post-info__autor">
-							<img class="news-post-info-autor__avatar" src="./static/images/images__home/ManWW.png" alt="img">
-							<span class="news-post-info-autor__name">William Wong</span>
-						</div>
-						<span class="news-post-info__release-date">September 25, 2015</span>
-					</div>
-				</div>
-			</div>
+			<?php
+			foreach ($futered_posts as $post) {
+				include 'futered_post.php';
+			}
+			?>
 		</div>
 		<div class="section-headings">
 			<span class="section-headings__title">Most Recent</span>
 		</div>
 		<div class="user-actions">
 			<div class="user-action-posts">
-				<div class="user-actions__post">
-					<img class="user-actions-post__background" src="./static/images/images__home/SST.jpg" alt="img">
-					<div class="user-actions-post__description">
-						<a href="#" class="user-actions-post__title">Still Standing Tall</a>
-						<span class="user-actions-post__subtitle">Life begins at the end of your comfort zone.</span>
-					</div>
-					<div class="user-actions-post__info">
-						<div class="user-actions-post-info__autor">
-							<img class="user-actions-post-autor__avatar" src="./static/images/images__home/ManWW.png" alt="img">
-							<span class="user-actions-post-autor__name">William Wong</span>
-						</div>
-						<span class="user-actions-post-info__publish-data">9/25/2015</span>
-					</div>
-				</div>
-				<div class="user-actions__post">
-					<img class="user-actions-post__background" src="./static/images/images__home/SSU.png" alt="img">
-					<div class="user-actions-post__description">
-						<a href="#" class="user-actions-post__title">Sunny Side Up</a>
-						<span class="user-actions-post__subtitle">No place is ever as bad as they tell you it’s going to
-							be.</span>
-					</div>
-					<div class="user-actions-post__info">
-						<div class="user-actions-post-info__autor">
-							<img class="user-actions-post-autor__avatar" src="./static/images/images__home/ManMV.png" alt="img">
-							<a href="#" class="user-actions-post-autor__name">Mat Vogels</a>
-						</div>
-						<span class="user-actions-post-info__publish-data">9/25/2015</span>
-					</div>
-				</div>
-				<div class="user-actions__post">
-					<img class="user-actions-post__background" src="./static/images/images__home/WF.png" alt="img">
-					<div class="user-actions-post__description">
-						<a href="#" class="user-actions-post__title">Water Falls</a>
-						<span class="user-actions-post__subtitle">We travel not to escape life, but for life not to
-							escape
-							us.</span>
-					</div>
-					<div class="user-actions-post__info">
-						<div class="user-actions-post-info__autor">
-							<img class="user-actions-post-autor__avatar" src="./static/images/images__home/ManMV.png" alt="img">
-							<a href="#" class="user-actions-post-autor__name">Mat Vogels</a>
-						</div>
-						<span class="user-actions-post-info__publish-data">9/25/2015</span>
-					</div>
-				</div>
-				<div class="user-actions__post">
-					<img class="user-actions-post__background" src="./static/images/images__home/TTM.png" alt="img">
-					<div class="user-actions-post__description">
-						<a href="#" class="user-actions-post__title">Through the Mist</a>
-						<span class="user-actions-post__subtitle">Travel makes you see what a tiny place you occupy in
-							the world.</span>
-					</div>
-					<div class="user-actions-post__info">
-						<div class="user-actions-post-info__autor">
-							<img class="user-actions-post-autor__avatar" src="./static/images/images__home/ManWW.png" alt="img">
-							<span class="user-actions-post-autor__name">William Wong</span>
-						</div>
-						<span class="user-actions-post-info__publish-data">9/25/2015</span>
-					</div>
-				</div>
-				<div class="user-actions__post">
-					<img class="user-actions-post__background" src="./static/images/images__home/AE.png" alt="img">
-					<div class="user-actions-post__description">
-						<a href="#" class="user-actions-post__title">Awaken Early</a>
-						<span class="user-actions-post__subtitle">Not all those who wander are lost.</span>
-					</div>
-					<div class="user-actions-post__info">
-						<div class="user-actions-post-info__autor">
-							<img class="user-actions-post-autor__avatar" src="./static/images/images__home/ManMV.png" alt="img">
-							<a href="#" class="user-actions-post-autor__name">Mat Vogels</a>
-						</div>
-						<span class="user-actions-post-info__publish-data">9/25/2015</span>
-					</div>
-				</div>
-				<div class="user-actions__post">
-					<img class="user-actions-post__background" src="./static/images/images__home/TIA.png" alt="img">
-					<div class="user-actions-post__description">
-						<a href="#" class="user-actions-post__title">Try it Always</a>
-						<span class="user-actions-post__subtitle">The world is a book, and those who do not travel read
-							only one page.</span>
-					</div>
-					<div class="user-actions-post__info">
-						<div class="user-actions-post-info__autor">
-							<img class="user-actions-post-autor__avatar" src="./static/images/images__home/ManMV.png" alt="img">
-							<a href="#" class="user-actions-post-autor__name">Mat Vogels</a>
-						</div>
-						<span class="user-actions-post-info__publish-data">9/25/2015</span>
-					</div>
-				</div>
+				<?php
+				foreach ($most_posts as $post) {
+					include 'post_preview.php';
+				}
+				?>
 			</div>
 		</div>
 	</main>
 	<footer class="footer">
 		<div class="navigation">
-			<a class="navigation__logo" href="#">Escape.</a>
-			<ul class="navigation__list">
+			<a class="navigation__logo_foot" href="#">
+				<img src="./static/images/images__home/Escape2.svg">
+			</a>
+			<ul class="navigation__list navigation__list_foot">
 				<li>
 					<a class="navigation__link" href="#">HOME</a>
 				</li>
